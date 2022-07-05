6a463b7396507ca3e9944ea4e3b6ad05dbc38882
Rami <103028944+Rami24t@users.noreply.github.com>
2022-07-05 12:05:16 +0200

    Update solution.js

---
+++ solution.js
@@ -0,0 +1,14 @@
+//1.
+let introSentence = 'intro ';
+let age = 32;
+console.log(introSentence + age);
+// + is concatenating the string and the number into a new string.
+
+//2.
+let integer = "1005";
+console.log(+ integer);
+
+let float = "10.05";
+console.log(+ "10.05");
+
+
